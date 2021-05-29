dd9a20fb434d4f04dbd3c425f907a3e24a7de20d
OsorioTiago <santiago7013@gmail.com>
2021-05-29 17:52:08 -0600

    Vista del login casi finalizada

--- src/view/viewLogin.java
+++ src/view/viewLogin.java
@@ -5,14 +5,27 @@ package view;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+
 import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.Cursor;
 
 public class viewLogin {
 
 	public JFrame frmLogin;
 	public JTextField textUser;
-	public JTextField textPassword;
+	public JPasswordField textPassword;
 	public JButton btnLogin;
 	
 	
@@ -21,6 +34,7 @@ public class viewLogin {
 	 */
 	public viewLogin() {
 		initialize();
+		textUser.setFocusable(true);
 	}
 
 	/**
@@ -28,33 +42,101 @@ public class viewLogin {
 	 */
 	public void initialize() {
 		frmLogin = new JFrame();
+		frmLogin.getContentPane().setBackground(Color.decode("#ec5353"));
+		frmLogin.setResizable(false);
 		frmLogin.setTitle("Login");
-		frmLogin.setBounds(100, 100, 450, 300);
+		frmLogin.setBounds(100, 100, 800, 503);
 		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmLogin.getContentPane().setLayout(null);
 		
 		textUser = new JTextField();
-		textUser.setBounds(186, 74, 147, 34);
+		textUser.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				
+				if(textUser.getText().trim().toLowerCase().equals("username")) {
+				  textUser.setText("");
+				  textUser.setForeground(Color.black);
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (textUser.getText().trim().equals("") || textUser.getText().trim().toLowerCase().equals("username")) {
+					  textUser.setText("username"); 
+					  textUser.setForeground(Color.GRAY); 
+				}
+			}
+			});
+		textUser.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		textUser.setForeground(Color.GRAY);
+		textUser.setText("username");
+		textUser.setBounds(139, 174, 190, 34);
 		frmLogin.getContentPane().add(textUser);
 		textUser.setColumns(10);
 		
-		JLabel lbUser = new JLabel("User:");
+		JLabel lbUser = new JLabel();
 		lbUser.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lbUser.setBounds(91, 67, 101, 36);
+		lbUser.setBounds(21, 174, 92, 54);
 		frmLogin.getContentPane().add(lbUser);
 		
-		JLabel lbPassword = new JLabel("Password:");
+		ImageIcon imgUser = new ImageIcon(getClass().getResource("/img/Sample_User_Icon.png")); 
+	    Icon fondoUser = new ImageIcon( imgUser.getImage().getScaledInstance(lbUser.getWidth(),lbUser.getHeight(), Image.SCALE_DEFAULT)); 
+	    lbUser.setIcon(fondoUser);
+		
+		JLabel lbPassword = new JLabel();
 		lbPassword.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lbPassword.setBounds(46, 137, 136, 36);
+		lbPassword.setBounds(31, 286, 82, 54);
 		frmLogin.getContentPane().add(lbPassword);
 		
-		textPassword = new JTextField();
+		ImageIcon impPass = new ImageIcon(getClass().getResource("/img/LockIconReduct (1).png")); 
+	    Icon fondoPass = new ImageIcon( impPass.getImage().getScaledInstance(lbPassword.getWidth(),lbPassword.getHeight(), Image.SCALE_DEFAULT)); 
+	    lbPassword.setIcon(fondoPass);
+		
+		textPassword = new JPasswordField();
+		textPassword.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(String.valueOf(textPassword.getPassword()).trim().equals("password")) {
+					textPassword.setText("");
+					textPassword.setForeground(Color.black); 
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(String.valueOf(textPassword.getPassword()).trim().equals("")) {
+					textPassword.setText("password");
+					textPassword.setForeground(Color.GRAY);
+				}
+			}
+		});
+		textPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		textPassword.setForeground(Color.GRAY);
+		textPassword.setText("password");
 		textPassword.setColumns(10);
-		textPassword.setBounds(186, 139, 147, 34);
+		textPassword.setBounds(139, 286, 190, 34);
 		frmLogin.getContentPane().add(textPassword);
 		
 		btnLogin = new JButton("Login");
-		btnLogin.setBounds(177, 203, 89, 23);
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnLogin.setBounds(139, 366, 190, 54);
 		frmLogin.getContentPane().add(btnLogin);
+		
+		JPanel panelIcon = new JPanel();
+		panelIcon.setBackground(Color.WHITE);
+		panelIcon.setBounds(389, 0, 407, 475);
+		frmLogin.getContentPane().add(panelIcon);
+		panelIcon.setLayout(null);
+		
+		JLabel lblIcon = new JLabel("");
+		lblIcon.setBounds(10, 120, 387, 244);
+		panelIcon.add(lblIcon);
+		
+		ImageIcon imagen1 = new ImageIcon(getClass().getResource("/img/starbuzzLogoSlogan.png")); 
+	    Icon fondo1 = new ImageIcon( imagen1.getImage().getScaledInstance(lblIcon.getWidth(),lblIcon.getHeight(), Image.SCALE_DEFAULT)); 
+	    lblIcon.setIcon(fondo1);
 	}
 }
