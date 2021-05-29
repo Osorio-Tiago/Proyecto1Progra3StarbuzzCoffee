fe8bffaf8121533a34ce15cc67087d292e8af38b
OsorioTiago <santiago7013@gmail.com>
2021-05-29 17:50:43 -0600

    Error de clave
    Adisión de un try catch donde se conecta a la DB porque estoy pendejo y
    configuré la mía con una clave distinta a "1234"

--- src/model/Database.java
+++ src/model/Database.java
@@ -12,6 +12,7 @@ public class Database {
 	private String base = "proyecto";
 	private String user  = "root";
 	private String password = "1234";
+	private String passwordTiago = "7060";
 	private String url = "jdbc:mysql://localhost:3306/" + base;
 	private Connection con = null; //La variable obtiene la conexion y la guarda
 	
@@ -20,7 +21,13 @@ public class Database {
 		try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver"); // Controlador para realizar conexion
-			con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+			
+			try {
+				con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+			}catch(SQLException e) {
+				con = (Connection) DriverManager.getConnection(this.url, this.user, this.passwordTiago);
+			}
+			
 			
 		} catch (SQLException e) {
 			System.out.println("Error in the connection with the DB" + e);
