21e5d714e49a69751b0a7f9f8b93abb0be7665df
OsorioTiago <santiago7013@gmail.com>
2021-06-02 21:10:53 -0600

    Implmentacion del patron SINGLETON

--- src/controller/ControlMenu.java
+++ src/controller/ControlMenu.java
@@ -1,15 +1,43 @@
 package controller;
 
+
+
 import view.viewMenu;
 
-public class ControlMenu {
+public class ControlMenu{
 
+	private controlUsers controlUsers = null;
+	private viewMenu viewMenu = new viewMenu();
+	
+	private static ControlMenu singletonInstance = null;
+	
+	
+	public static ControlMenu getControlMenu(){
+		if(singletonInstance == null){
+			singletonInstance = new ControlMenu();
+		}
+	    return singletonInstance;
+    }
+	   
+	public void setcontrolUsers(controlUsers controlUsers) {
+		this.controlUsers = controlUsers;
+	}
+	public void start() {
+
+		viewMenu.frmStarbuzzCoffee.setTitle("StarBuzz Coffee");
+		viewMenu.frmStarbuzzCoffee.setLocationRelativeTo(null);
+	}
+	
 	public void StartApplication() {
 		try {
-			viewMenu window = new viewMenu();
-			window.frmStarbuzzCoffee.setVisible(true);
+			
+			viewMenu.frmStarbuzzCoffee.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void StartViewUsers() {
+		controlUsers.start();
 	}
 }
