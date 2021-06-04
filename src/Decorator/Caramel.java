e6bbb97275b968bfe8d562927df537366375b570
Ariadna_Ch <ariadna.c2405@gmail.com>
2021-06-03 18:25:31 -0600

    Ingrediente

---
+++ src/Decorator/Caramel.java
@@ -0,0 +1,20 @@
+package Decorator;
+
+public class Caramel extends BeverageDecorator{
+	
+	 public Caramel(IBeverage beverage) {
+	        super(beverage);
+	 }
+	 
+	 //Le suma al precio base de la bebida el costo del ingrediente adicional.
+	 public double cost(){
+	        return super.cost() + 1.80;
+	 }
+	    
+	 public String createBeverage(){
+	        return super.createBeverage() + "\nAdditional Ingredient: Caramel -> $1.80";
+	 }
+	    
+	
+
+}
