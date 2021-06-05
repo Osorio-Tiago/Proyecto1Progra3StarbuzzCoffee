947f29dfdee6e6e74c6e1d295ed4bd349916e1cd
Ariadna_Ch <ariadna.c2405@gmail.com>
2021-06-04 22:01:38 -0600

    Cambio comentario

--- src/decorator/BeverageDecorator.java
+++ src/decorator/BeverageDecorator.java
@@ -1,7 +1,8 @@
 package decorator;
 
-//Implementación de la interface IBeverage. Esta clase define el objeto
-//que posteriormente será decorado.
+//Clase abstracta que especifica la estructura elemental de un decorador,
+//la cual a su vez hereda de IBeverage y contiene un atributo de dicha interface 
+//que podrá ser decorado.
 
 public abstract class BeverageDecorator implements IBeverage {
 	 protected IBeverage beverage;
